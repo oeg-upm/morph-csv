0a6a588837c7e010ff86cc235a01ee07605e1f05
Jhon <jhontb7@gmail.com>
2019-11-12 11:39:57 +0000

    added some comments

--- csvwParser/code/main.py
+++ csvwParser/code/main.py
@@ -10,23 +10,34 @@ def scriptCaller(data):
     replaceDelimiter(parser.getDelimiter(data), url)
     dateFormatReplacer(parser.getDateFormat(data), url)
 
+'''
+Insert row titles (from csvw:rowTitles)
+'''
 ##Insert the titles of 
 def insertTitles(data, path):
     #print("Titles: " + data)
     #data=data.replace(" ", "")
     os.system('./bashScripts/insertTitles.sh \'%s\' %s'%(data, path))
 
-
+'''
+#Skip rows (csvw:skipRows → remove the first n rows)
+'''
 #If the skipRow is bigger than 0 the function execute the BashScritp
 def rowSkipper(data, path):
     if(int(data)> 0):
         os.system('./bashScripts/skipRows.sh %s %s'%(data, path))
 
+'''
+#Delimiter (csvw:delimiter → change to comma)
+'''
 def replaceDelimiter(data, path):
     delimiter = str(data.encode('utf-8'))[1:]
     #print("Delimiter: " + delimiter + " File: " + path)
     os.system('./bashScripts/delimiterReplacer.sh %s %s'%(delimiter, path))
 
+'''
+Dates and booleans format (csvw:format → sql formats)
+'''
 def dateFormatReplacer(data, path):
     print(data)
 def main():
