53be2d96b5a3de66683b6095f3a7972c3bd48abc
Jhon T <jhontb7@gmail.com>
2019-11-19 16:19:36 +0100

    added null format

--- csvwParser/code/bashScripts/nullFormatChanger.sh
+++ csvwParser/code/bashScripts/nullFormatChanger.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+all='$0'
 null=$1
-col=$2
+col="$"$2
 file=$3
 #echo COL:$col NULL:$null FILE:$file
-cat ./tmp/$file | cut -d ',' -f$col | sed  -r -e "s/,$null,/,null,/"
+#cat ./tmp/$file | cut -d ',' -f$col | sed  -r -e "s/,$null,/,null,/"
+#echo awk -F'\",\"' '{gsub(/\-/,"null",$5); print $0 }' tmp/gene_info
+awk -F'\",\"' "{gsub(/\\$null/,\"null\",$col); print $all }" tmp/$file 
