e55702334f865f791b39b004b7c8479458c0d0ef
David Chaves <dachafra@gmail.com>
2019-11-19 14:58:40 +0100

    def main in main

--- main.py
+++ main.py
@@ -1,45 +1,38 @@
 
-
 from selection.readRMLTriplesMap import mapping_parser
 from selection.yarrrml2rml import yarrrml2rml
 from selection.selectMappingFromSPARQL import fromSPARQLtoMapping
+import argparse
 import sys
 
-if __name__ == "__main__":
 
-    print("Downloading mappings, queries and data based on config.json")
-
+def main():
     print("Inserting headers in CSV files")
-
 
     print("Removing FnO functions from RML")
     functions, mapping = yarrrml2rml("./tmp/mapping.yml")
 
     print("Selecting RML rules for answering the query")
-    #this function creates the rml rules needed to answer query from yarrrml mapping
+    # this function creates the rml rules needed to answer query from yarrrml mapping
     fromSPARQLtoMapping(mapping, "query")
 
-
     print("Selecting CSV files for answering the query")
-    #get the triplesMaps objects from RML
+    # get the triplesMaps objects from RML
     triples_map_list = mapping_parser("./tmp/mapping.rml.ttl")
-    #obtain the CSV and columns based on mapping
+    # obtain the CSV and columns based on mapping
     # ToDo: Freddy here insert your functions
 
     print("Cleaning CSV files based on CSVW")
-    #cleaning stuff
-
+    # cleaning stuff
 
     print("Normalizing CSV files")
-    #normalize
-
+    # normalize
 
     print("Creating new columns based on FnO functions on RML")
 
-
-
     print("Removing duplicates")
 
-
-
     print("Answering query")
+
+if __name__ == "__main__":
+    main()
