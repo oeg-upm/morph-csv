fc438eafd7d2c96444e153b52f6fc60caf4a0c17
David Chaves <dachafra@gmail.com>
2019-11-20 12:14:31 +0100

    utils v1.0

---
+++ utils/utils-resources.py
@@ -0,0 +1,41 @@
+import re
+import os
+
+
+def downloadAnnotations(config):
+    for key in config:
+        source = config[key]
+        if re.match("^http.*", source):
+            filename = source.split("/")[len(source.split("/")) - 1]
+            os.system("wget -O ./tmp/annotations/" + filename + " " + source)
+        else:
+            os.system("mv " + source + " ./tmp/annotations/")
+
+
+def downloadCSVfilesFromRML(mapping):
+    for tm in mapping["mappings"]:
+        source = mapping["mappings"][tm]["sources"][0][0]
+        if re.match("^http.*", source):
+            filename = re.sub("~csv", "", mapping["mappings"][tm]["sources"][0][0].split("/")[
+                len(mapping["mappings"][tm]["sources"][0][0].split("/")) - 1])
+            os.system("wget -O ./tmp/csv/" + filename + " " + re.sub("~csv", source))
+        else:
+            os.system("mv " + source + " ./tmp/csv/")
+
+
+def readQuery(path):
+    with open(path, "r") as file:
+        content = file.read()
+    return content
+
+
+def maketmpdirs():
+    os.system("mkdir ./tmp/")
+    os.system("mkdir ./tmp/annotations")
+    os.system("mkdir ./tmp/csv")
+    os.system("mkdir ./tmp/morph-properties")
+    os.system("mkdir ./results")
+
+
+def removetmpdirs():
+    os.system("rm -r ./tmp")
