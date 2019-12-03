aa756d408643337543567d3aa20505100ef6943a
Jhon T <jhontb7@gmail.com>
2019-12-03 17:52:23 +0100

    run

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #source=$1
 #query=$2 
-#python3 morphcsv.py -c test/config.json -q test/query.rq
-python3 morphcsv.py  -c test/configGtfs.json -q test/queryGtfs.rq
-
+python3 morphcsv.py -c test/config.json -q test/query.rq
+#python3 morphcsv.py  -c test/configGtfs.json -q test/queryGtfs.rq
