24818bcaae517020d06f12ac4f81574cf685fc72
Jhon Toledo <jhontb7@gmail.com>
2019-12-08 23:10:42 +0100

    added some comments

--- schema_generation/create_and_insert.py
+++ schema_generation/create_and_insert.py
@@ -3,9 +3,12 @@ import psycopg2
 import re
 import os
 def create_and_insert(csvw,sql):
-    #con = psycopg2.connect(database="morphcsv", user="user", password="csv", host="127.0.0.1", port="5432")
+
     try:
+        # Local connection
         #con = psycopg2.connect(database="morphcsv", user="postgres", password="csv", host="127.0.0.1", port="5432")
+
+        #Docker connection
         con = psycopg2.connect(database="morphcsv", user="user", password="csv", host="postgres")
     except:
         print("I am unable to connect to the database.")
@@ -22,9 +25,12 @@ def insert_data(csvw,con):
     #for i, table in enumerate(csvw["tables"]):
     for i,table in enumerate(csvw["tables"]):
         tablename = re.sub(".csv", "", csvw["tables"][i]["url"].split("/")[-1])
-        pwd = os.getcwd()
+
+        # Insert docker db
         insert = "COPY " + tablename + " FROM '/tmp/csv/" + tablename + ".csv' with NULL as E'null' CSV HEADER;"
-        #insert = "COPY " + tablename + " FROM '/tmp/csv/" + tablename + ".csv' with CSV HEADER;"
+
+        #Insert local db
+        #pwd = os.getcwd()
         #insert = "COPY "+tablename+" FROM '" + str(pwd) + "/tmp/csv/" + tablename + ".csv' with NULL as E'null' CSV HEADER;"
 
         cur = con.cursor()
