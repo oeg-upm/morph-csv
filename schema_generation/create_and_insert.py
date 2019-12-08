90448f0c8bc4ff7b3b0cf1f9c9efcca3269f8fa3
Jhon Toledo <jhontb7@gmail.com>
2019-12-08 20:42:53 +0100

    Update create_and_insert.py

--- schema_generation/create_and_insert.py
+++ schema_generation/create_and_insert.py
@@ -5,8 +5,8 @@ import os
 def create_and_insert(csvw,sql):
     #con = psycopg2.connect(database="morphcsv", user="user", password="csv", host="127.0.0.1", port="5432")
     try:
-        con = psycopg2.connect(database="morphcsv", user="postgres", password="csv", host="127.0.0.1", port="5432")
-        #con = psycopg2.connect(database="morphcsv", user="user", password="csv", host="postgres")
+        #con = psycopg2.connect(database="morphcsv", user="postgres", password="csv", host="127.0.0.1", port="5432")
+        con = psycopg2.connect(database="morphcsv", user="user", password="csv", host="postgres")
     except:
         print("I am unable to connect to the database.")
         sys.exit()
@@ -23,9 +23,9 @@ def insert_data(csvw,con):
     for i,table in enumerate(csvw["tables"]):
         tablename = re.sub(".csv", "", csvw["tables"][i]["url"].split("/")[-1])
         pwd = os.getcwd()
-        #insert = "COPY " + tablename + " FROM '/tmp/csv/" + tablename + ".csv' with NULL as E'null' CSV HEADER;"
+        insert = "COPY " + tablename + " FROM '/tmp/csv/" + tablename + ".csv' with NULL as E'null' CSV HEADER;"
         #insert = "COPY " + tablename + " FROM '/tmp/csv/" + tablename + ".csv' with CSV HEADER;"
-        insert = "COPY "+tablename+" FROM '" + str(pwd) + "/tmp/csv/" + tablename + ".csv' with NULL as E'null' CSV HEADER;"
+        #insert = "COPY "+tablename+" FROM '" + str(pwd) + "/tmp/csv/" + tablename + ".csv' with NULL as E'null' CSV HEADER;"
 
         cur = con.cursor()
         cur.execute(insert)
