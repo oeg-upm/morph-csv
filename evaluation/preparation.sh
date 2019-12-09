af5d45822a44193f13103908d71a7cae57f832f5
David Chaves <dachafra@gmail.com>
2019-12-09 17:38:53 +0100

    preparation script

--- evaluation/preparation.sh
+++ evaluation/preparation.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 cd ..
 cp -r /morphcsv/evaluation/engines/* /morphcsv/tmp/
-mv /morphcsv/tmp/morph-rdb.properties /morphcsv/tmp/csv/
 cd /morphcsv/tmp/
 wget https://github.com/ontop/ontop/releases/download/ontop-3.0.0/ontop-cli-3.0.0.zip
-unzip ontop-cli-3.0.0.zip
+unzip ontop-cli-3.0.0.zip -d ontop
 mv /morphcsv/tmp/run-ontop.sh /morphcsv/tmp/ontop/
 cd /morphcsv/tmp/ontop/jdbc/
 wget https://jdbc.postgresql.org/download/postgresql-42.2.9.jar
@@ -16,15 +15,15 @@ cp /morphcsv/evaluation/gtfs/gtfs.csvw.json /mappings/
 cp /morphcsv/evaluation/gtfs/gtfs-csv.yaml /mappings/
 cp /morphcsv/evaluation/gtfs/config-gtfs.json /mappings/
 mkdir /queries/gtfs
-cp -r /morphcsv/evaluation/gtfs/queries/ /queries/gtfs/
+cp -r /morphcsv/evaluation/gtfs/queries/* /queries/gtfs/
 cp /morphcsv/evaluation/gtfs/evaluate.sh /evaluate-gtfs.sh
 
 #bio2rdf
 cp /morphcsv/evaluation/bio2rdf/bio2rdf.csvw.json /mappings/
-cp /morphcsv/evaluation/gtfs/bio2rdf.yml /mappings/
-cp /morphcsv/evaluation/gtfs/config-bio2rdf.json /mappings/
+cp /morphcsv/evaluation/bio2rdf/bio2rdf.yml /mappings/
+cp /morphcsv/evaluation/bio2rdf/config-bio2rdf.json /mappings/
 mkdir /queries/bio2rdf
-cp -r /morphcsv/evaluation/bio2rdf/queries/ /queries/bio2rdf/
-cp /morphcsv/evaluation/gtfs/evaluate.sh /evaluate-bio2rdf.sh
+cp -r /morphcsv/evaluation/bio2rdf/queries/* /queries/bio2rdf/
+cp /morphcsv/evaluation/bio2rdf/evaluate.sh /evaluate-bio2rdf.sh
 #data
 #use the csv with links inside the evaluation folders to download the data manually
