5cd3e615056d04ebdc8937a35e70223cde3058ce
w0xter <w0xter@github.com>
2020-02-19 12:23:25 +0100

    Minor Changes

--- dropDatabase.sh
+++ dropDatabase.sh
@@ -1,3 +1,3 @@
-sudo -u postgres -i psql -c "drop database if exists GTFS;"
+sudo -u postgres -i psql -c "drop database if exists morphcsv;"
 sudo systemctl restart postgresql
 echo "DATA BASE RESTARTED"
